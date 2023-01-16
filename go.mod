9c034ed9f22a3d9d2efd2e13826d717f5f45c852
lavia <ty.zhao@live.com>
2023-01-16 18:42:42 +0800

    sever暂未完成，依赖中有bug未修复

--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
 module github.com/1tex/gotrainingcamp
-
-go 1.19
---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/1tex/gotrainingcamp/repository"
+	"os"
+)
+
+func main() {
+	if err := Init("./data/"); err != nil {
+		os.Exit(-1)
+	}
+	r := gin.Default()
+	r.GET("/community/page/get/:id", func(c *gin.Context) {
+		topicId := c.Param("id")
+		data := cotroller.QueryPageInfo(topicId)
+		c.JSON(200, data)
+	})
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
+
+func Init(filePath string) error {
+	if err := repository.Init(filePath); err != nil {
+		return err
+	}
+	return nil
+}
